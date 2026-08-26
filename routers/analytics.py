615053c3cfebc777c481227e995e0820a3e685f8
Vishal Balaji <vishalsuji0711@gmail.com>
2026-08-26 12:35:51 +0530

    Added revenue analytics into the analytics dashboard

--- routers/analytics.py
+++ routers/analytics.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from database.db import SessionLocal
 
 from database.db import get_db
-from services.analytics_service import get_analytics
+from services.analytics_service import get_analytics, get_revenue_analytics
 from services.auth_service import get_current_user
 
 router = APIRouter(
@@ -17,3 +17,11 @@ def analytics(
     current_user = Depends(get_current_user)
 ):
     return get_analytics(db)
+
+
+@router.get("/revenue")
+def revenue_analytics(
+    db : Session = Depends(get_db), 
+    current_user = Depends(get_current_user)
+):
+    return get_revenue_analytics(db)
--- services/analytics_service.py
+++ services/analytics_service.py
@@ -3,6 +3,7 @@ from sqlalchemy import func
 
 from models.product_model import Product
 from models.order_item_model import OrderItem
+from models.order_model import Order
 
 def get_analytics(
         db : Session
@@ -46,4 +47,34 @@ def get_analytics(
 
     return {
         "best_selling_products" : best_products
+    }
+
+def get_revenue_analytics(
+        db : Session
+):
+    total_revenue = (
+        db.query(
+            func.sum(Order.total_amount)
+        )
+        .scalar()
+    )
+
+    total_orders = (
+        db.query(
+            func.count(Order.id)
+        )
+        .scalar()
+    )
+
+    average_order_value = (
+        db.query(
+            func.avg(Order.total_amount)
+        )
+        .scalar()
+    )
+
+    return {
+        "total_revenue" : total_revenue or 0, 
+        "total_orders" : total_orders or 0, 
+        "average_order_value" : average_order_value or 0
     }
